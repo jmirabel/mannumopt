76ee80cc3ef275159e94428c89b228e04733b14b
Joseph Mirabel <jmirabel@laas.fr>
2021-03-09 14:55:44 +0100

    Fix second_order_central on manifolds.

--- include/mannumopt/numdiff.hpp
+++ include/mannumopt/numdiff.hpp
@@ -90,77 +90,117 @@ void central(Function& func, const Eigen::MatrixBase<InputVector>& x,
 template<typename Function, typename Integrate, typename InputVector, typename OutputDerivative>
 void second_order_central(Function& func, const Eigen::MatrixBase<InputVector>& x,
     const typename InputVector::Scalar eps,
-    const Eigen::MatrixBase<OutputDerivative>& fx_out,
+    const Eigen::MatrixBase<OutputDerivative>& fxx_out,
     Integrate integrate)
 {
   typedef Eigen::Matrix<typename OutputDerivative::Scalar,
     OutputDerivative::ColsAtCompileTime, 1> InputTangent;
   typedef typename Function::Output OutputVector;
 
-  OutputDerivative& fx = const_cast<OutputDerivative&>(fx_out.derived());
+  OutputDerivative& fxx = const_cast<OutputDerivative&>(fxx_out.derived());
   OutputVector v_x;
-  InputTangent t (InputTangent::Zero(fx.cols()));
-  InputVector x_t (x);
+  InputTangent t (InputTangent::Zero(fxx.cols()));
+  InputVector x_t (x.size()), x_m (x.size()), x_p (x.size());
 
-  func.f(x, v_x);
-
-  fx.diagonal().array() = -2*v_x;
   auto eps2 = eps*eps;
 
-  for (int i = 0; i < fx.cols(); ++i) {
-    t[i] = -eps;
-    integrate(x_t, x, t);
-    func.f(x_t, v_x);
-    fx(i,i) += v_x;
-
-    t[i] = eps;
-    integrate(x_t, x, t);
-    func.f(x_t, v_x);
-    fx(i,i) += v_x;
-
-    fx(i,i) /= eps2;
-
-    for (int j = i+1; j < fx.cols(); ++j) {
-      t[i] = eps/2;
-
+  for (int i = 0; i < fxx.cols(); ++i) {
+    // xp
+    t[i] =  eps/2;
+    integrate(x_p, x, t);
+    t[i] = -eps/2;
+    integrate(x_m, x, t);
+    t[i] = 0.;
+    for (int j = 0; j < fxx.rows(); ++j) {
+      // xp
       t[j] = eps/2;
-      integrate(x_t, x, t);
+      integrate(x_t, x_p, t);
       func.f(x_t, v_x);
-      fx(i,j) = v_x;
-
+      fxx(j,i)  = v_x;
       t[j] = -eps/2;
-      integrate(x_t, x, t);
+      integrate(x_t, x_p, t);
       func.f(x_t, v_x);
-      fx(i,j) -= v_x;
-
-      t[i] = -eps/2;
-
+      fxx(j,i) -= v_x;
+      // xm
       t[j] = eps/2;
-      integrate(x_t, x, t);
+      integrate(x_t, x_m, t);
       func.f(x_t, v_x);
-      fx(i,j) -= v_x;
-
+      fxx(j,i) -= v_x;
       t[j] = -eps/2;
-      integrate(x_t, x, t);
+      integrate(x_t, x_m, t);
       func.f(x_t, v_x);
-      fx(i,j) += v_x;
+      fxx(j,i) += v_x;
 
-      fx(i,j) /= eps2;
-      fx(j,i) = fx(i,j);
-
+      fxx(j,i) /= eps2;
       t[j] = 0;
     }
-    t[i] = 0;
   }
 }
 
 template<typename Function, typename InputVector, typename OutputDerivative>
 void second_order_central(Function& func, const Eigen::MatrixBase<InputVector>& x,
     const typename InputVector::Scalar eps,
-    const Eigen::MatrixBase<OutputDerivative>& fx_out)
+    const Eigen::MatrixBase<OutputDerivative>& fxx_out)
 {
-  auto integrate = &mannumopt::internal::vector_space_addition<typename InputVector::Scalar, InputVector::RowsAtCompileTime>;
-  second_order_central(func, x, eps, fx_out, integrate);
+  typedef Eigen::Matrix<typename OutputDerivative::Scalar,
+    OutputDerivative::ColsAtCompileTime, 1> InputTangent;
+  typedef typename Function::Output OutputVector;
+
+  OutputDerivative& fxx = const_cast<OutputDerivative&>(fxx_out.derived());
+  OutputVector v_x;
+  InputTangent t (InputTangent::Zero(fxx.cols()));
+  InputVector x_t (x);
+
+  func.f(x, v_x);
+
+  fxx.diagonal().array() = -2*v_x;
+  auto eps2 = eps*eps;
+
+  for (int i = 0; i < fxx.cols(); ++i) {
+    t[i] = -eps;
+    x_t = x + t;
+    func.f(x_t, v_x);
+    fxx(i,i) += v_x;
+
+    t[i] = eps;
+    x_t = x + t;
+    func.f(x_t, v_x);
+    fxx(i,i) += v_x;
+
+    fxx(i,i) /= eps2;
+
+    for (int j = i+1; j < fxx.cols(); ++j) {
+      t[i] = eps/2;
+
+      t[j] = eps/2;
+      x_t = x + t;
+      func.f(x_t, v_x);
+      fxx(i,j) = v_x;
+
+      t[j] = -eps/2;
+      x_t = x + t;
+      func.f(x_t, v_x);
+      fxx(i,j) -= v_x;
+
+      t[i] = -eps/2;
+
+      t[j] = eps/2;
+      x_t = x + t;
+      func.f(x_t, v_x);
+      fxx(i,j) -= v_x;
+
+      t[j] = -eps/2;
+      x_t = x + t;
+      func.f(x_t, v_x);
+      fxx(i,j) += v_x;
+
+      fxx(i,j) /= eps2;
+      fxx(j,i) = fxx(i,j);
+
+      t[j] = 0;
+    }
+    t[i] = 0;
+  }
 }
 
 } // namespace numdiff
